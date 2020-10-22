a1547eb9ec0a8a3425c36215b4986edab59c942f
brandon-perry <brandonperryofficial@gmail.com>
2020-10-22 15:53:40 -0400

    finished phase 1

--- app.js
+++ app.js
@@ -1,13 +1,59 @@
 const express = require('express');
 
+const morgan = require('morgan');
+
 const routes = require('./routes');
 
 const app = express();
 
+app.use(morgan('dev'));
+
 app.set('view engine', 'pug');
 
-app.use(routes)
+app.use(routes);
 
-const port = 8080;
+app.get('/favicon.ico', (req, res) => {
+    res.send();
+    }); 
 
-app.listen(port,() => console.log(`Listening on port ${port}...`))
+//Errors
+
+app.use((req, res, next)=> {
+
+    const err = new Error('The requested page couldnt be found.');
+    err.status = 404;
+    next(err)
+});
+
+app.use((err, req, res, next) => {
+    if (process.env.NODE_ENV === 'production') {
+        //todo log the erro to the database
+    } else {
+        console.error(err)
+    }
+    next(err)
+})
+
+app.use((err, req, res, next) => {
+    if (err.status === 404) {
+        res.status(404);
+        res.render('page-not-found', {
+            title: 'Page Not Found',
+        })
+    } else {
+        next(err)
+    }
+})
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    const isProduction = process.env.NODE_ENV === 'production';
+    res.render('error', {
+        title: 'Server Error',
+        message: isProduction ? null : err.message,
+        stack: isProduction ? null : err.stack,
+    })
+})
+
+
+module.exports = app;
